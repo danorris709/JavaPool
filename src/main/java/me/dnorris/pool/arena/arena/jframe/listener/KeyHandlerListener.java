8fb59160f9ee4d585b4856c54030fbc5acb19da7
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-04-24 11:36:34 +0100

    Feat: Added KeyHandlerListener

---
+++ src/main/java/me/dnorris/pool/arena/arena/jframe/listener/KeyHandlerListener.java
@@ -0,0 +1,42 @@
+package me.dnorris.pool.arena.arena.jframe.listener;
+
+import me.dnorris.pool.arena.GameFunction;
+import me.dnorris.pool.arena.arena.jframe.JFrameArena;
+import me.dnorris.pool.arena.key.KeyEventType;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandlerListener implements KeyListener {
+
+    private final JFrameArena gameArena;
+
+    public KeyHandlerListener(JFrameArena gameArena) {
+        this.gameArena = gameArena;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent event) {
+        this.attemptRun(event, KeyEventType.KEY_TYPED);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent event) {
+        this.attemptRun(event, KeyEventType.KEY_PRESSED);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent event) {
+        this.attemptRun(event, KeyEventType.KEY_RELEASED);
+    }
+
+    private void attemptRun(KeyEvent event, KeyEventType type) {
+        for (GameFunction handler : this.gameArena.getHandlers(event.getKeyCode())) {
+            if(handler.getEventType() != type) {
+                continue;
+            }
+
+            handler.getFunction().run(this.gameArena, this.gameArena, event);
+        }
+    }
+}
