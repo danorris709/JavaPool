435e15d4629549a9c1a3f627064eab4074dbc4fb
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-04-24 11:22:18 +0100

    Feat: Added Pair DTO

---
+++ src/main/java/me/dnorris/pool/data/Pair.java
@@ -0,0 +1,20 @@
+package me.dnorris.pool.data;
+
+public class Pair<A, B> {
+
+    private final A first;
+    private final B second;
+
+    public Pair(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public A getFirst() {
+        return this.first;
+    }
+
+    public B getSecond() {
+        return this.second;
+    }
+}
