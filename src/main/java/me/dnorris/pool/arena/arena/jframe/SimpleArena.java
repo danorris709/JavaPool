13a116fb8bd111af40cb5d649e3bfc3020fbccb9
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-04-25 13:45:30 +0100

    Fix: Arena now calls paint

--- src/main/java/me/dnorris/pool/arena/arena/jframe/SimpleArena.java
+++ src/main/java/me/dnorris/pool/arena/arena/jframe/SimpleArena.java
@@ -1,7 +1,9 @@
 package me.dnorris.pool.arena.arena.jframe;
 
+import me.dnorris.pool.arena.Entity;
 import me.dnorris.pool.arena.arena.AbstractGameArena;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class SimpleArena extends AbstractGameArena {
@@ -9,11 +11,26 @@ public class SimpleArena extends AbstractGameArena {
     public SimpleArena(Dimension dimensions) {
         super(dimensions);
 
-        new GameFrame(this);
+        JFrame jFrame = new GameFrame(this);
+
+        new Thread(() -> {
+            while(true) {
+                jFrame.repaint();
+
+                try {
+                    Thread.sleep(20);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     @Override
     public void tick(Graphics2D graphics) {
-        // TODO: 24/04/2020
+        for (Entity entity : this.getEntities()) {
+            entity.paint(graphics);
+            entity.tick();
+        }
     }
 }
