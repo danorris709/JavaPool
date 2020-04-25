6a93d0c9401f3907c921ce2a07bf30a63410b81a
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-04-25 13:44:16 +0100

    Feat: Added dimensions to hitbox

--- src/main/java/me/dnorris/pool/data/hitbox/Hitbox.java
+++ src/main/java/me/dnorris/pool/data/hitbox/Hitbox.java
@@ -2,9 +2,13 @@ package me.dnorris.pool.data.hitbox;
 
 import me.dnorris.pool.data.location.Location;
 
+import java.awt.*;
+
 public interface Hitbox {
 
     Location getCenter();
+
+    Dimension getDimensions();
 
     boolean isColliding(Hitbox other);
 
