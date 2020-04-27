be3c46cd274da7ef48824905df9680eb1438c5ba
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-04-27 18:46:16 +0100

    Feat: added compound entity

---
+++ src/main/java/me/dnorris/pool/arena/entity/CompoundEntity.java
@@ -0,0 +1,39 @@
+package me.dnorris.pool.arena.entity;
+
+import com.google.common.collect.Lists;
+import me.dnorris.pool.arena.Entity;
+import me.dnorris.pool.data.hitbox.Hitbox;
+import me.dnorris.pool.data.location.Location;
+import me.dnorris.pool.data.vector.Vector;
+
+import java.awt.*;
+import java.util.List;
+
+public abstract class CompoundEntity extends AbstractEntity {
+
+    private List<Entity> entities;
+
+    public CompoundEntity(Location centerLocation, Color colour, Vector motion, Hitbox hitbox, Entity... entities) {
+        super(colour, centerLocation, hitbox, motion, false);
+
+        this.entities = Lists.newArrayList(entities);
+    }
+
+    @Override
+    public void setColour(Color colour) {
+        super.setColour(colour);
+
+        for (Entity entity : this.entities) {
+            entity.setColour(colour);
+        }
+    }
+
+    @Override
+    public void setMotion(Vector motion) {
+        super.setMotion(motion);
+
+        for (Entity entity : this.entities) {
+            entity.setMotion(motion);
+        }
+    }
+}
