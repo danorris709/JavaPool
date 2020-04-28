f8176c32c584a32ec27eb1dbd2047c049d1de426
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-04-28 21:20:35 +0100

    Feat: Added interactability to hitboxes Ref: added comments to Hitbox interface

--- src/main/java/me/dnorris/pool/data/hitbox/Hitbox.java
+++ src/main/java/me/dnorris/pool/data/hitbox/Hitbox.java
@@ -6,14 +6,62 @@ import java.awt.*;
 
 public interface Hitbox {
 
+    /**
+     *
+     * Center {@link Location} of the {@link Hitbox}
+     *
+     * @return The center of the {@link Hitbox}
+     */
     Location getCenter();
 
+    /**
+     *
+     * The height and width of the {@link Hitbox}
+     *
+     * @return The {@link Dimension} for the {@link Hitbox}
+     */
     Dimension getDimensions();
 
+    /**
+     *
+     * Determines if the {@link Hitbox} can be moved
+     *
+     * @return If the {@link Hitbox} is move-able
+     */
     boolean isImmovable();
 
+    /**
+     *
+     * Update if the {@link Hitbox} is move-able
+     *
+     * @param immovable Can the {@link Hitbox} be moved
+     */
     void setImmovable(boolean immovable);
 
+    /**
+     *
+     * Determines if the {@link Hitbox} can be interacted with by other
+     * {@link Hitbox}
+     *
+     * @return If the {@link Hitbox} can be hit
+     */
+    boolean isInteractable();
+
+    /**
+     *
+     * Update if the {@link Hitbox} can be interacted with by other {@link Hitbox}
+     *
+     * @param interactable Can the {@link Hitbox} be hit
+     */
+    void setInteractable(boolean interactable);
+
+    /**
+     *
+     * Determine if the two {@link Hitbox} are interacting with eachother
+     *
+     * @param other The other {@link Hitbox} being checked
+     * @return If the two {@link Hitbox} have collided with eachother
+     */
     boolean isColliding(Hitbox other);
 
 }
