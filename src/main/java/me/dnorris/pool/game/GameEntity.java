3b11663673e93755745e491f19f7b5f6775d4280
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-04-28 22:07:45 +0100

    Feat: changed border to constant with updated HollowRectangle Builder

--- src/main/java/me/dnorris/pool/game/GameEntity.java
+++ src/main/java/me/dnorris/pool/game/GameEntity.java
@@ -2,35 +2,30 @@ package me.dnorris.pool.game;
 
 import me.dnorris.pool.arena.Entity;
 import me.dnorris.pool.arena.entity.EntityBuilder;
+import me.dnorris.pool.arena.entity.EntityType;
 import me.dnorris.pool.arena.entity.compound.shape.HollowRectangle;
 import me.dnorris.pool.data.Colour;
 import me.dnorris.pool.data.location.implementation.Location2D;
-import me.dnorris.pool.data.vector.implementation.Vector2D;
 
 import java.awt.*;
 import java.lang.reflect.InvocationTargetException;
 
 public class GameEntity {
 
-    private static Entity border;
+    private static Entity BORDER = new HollowRectangle.Builder()
+            .setLocation(new Location2D(100, 100))
+            .setColour(Colour.BROWN)
+            .setInteractable(true)
+            .setImmovable(true)
+            .setType(EntityType.HOLLOW_RECTANGLE)
+            .setDimension(new Dimension(500, 250))
+            .setOutsideWidth(20)
+            .build();
+
     private static Entity cloth;
 
     public static Entity getBorder() {
-        if(border == null) {
-            try {
-                border = new HollowRectangle.Builder()
-                        .setLocation(new Location2D(100, 100))
-                        .setColour(Colour.BROWN)
-                        .setInteractable(true)
-                        .setImmovable(true)
-                        .setDimension(new Dimension(500, 250))
-                        .build();
-            } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return border;
+        return BORDER;
     }
 
     public static Entity getCloth() {
@@ -43,6 +38,7 @@ public class GameEntity {
                         .setImmovable(true)
                         .setInteractable(false)
                         .setHollow(false)
+                        .setType(EntityType.RECTANGLE)
                         .build();
             } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
                 e.printStackTrace();
