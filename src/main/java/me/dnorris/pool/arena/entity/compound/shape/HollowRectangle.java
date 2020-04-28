ebc90fee61a7747b616692db7ab3483c2fde707a
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-04-28 21:09:42 +0100

    Feat: Started HollowRectangle

---
+++ src/main/java/me/dnorris/pool/arena/entity/compound/shape/HollowRectangle.java
@@ -0,0 +1,33 @@
+package me.dnorris.pool.arena.entity.compound.shape;
+
+import com.google.common.collect.Lists;
+import me.dnorris.pool.arena.entity.compound.CompoundEntity;
+import me.dnorris.pool.arena.entity.shape.RectangleEntity;
+import me.dnorris.pool.data.hitbox.Hitbox;
+import me.dnorris.pool.data.hitbox.implementation.SquareLocationHitbox;
+import me.dnorris.pool.data.location.Location;
+import me.dnorris.pool.data.vector.Vector;
+
+import java.awt.*;
+import java.util.List;
+
+public class HollowRectangle extends CompoundEntity {
+
+    public HollowRectangle(Location centerLocation, Color colour, Vector motion, int width, int height, int outsideWidth, boolean immovable) {
+        super(centerLocation, colour, motion, null,
+                createEntities(centerLocation, colour, width, height, outsideWidth, immovable));
+    }
+
+    private static RectangleEntity[] createEntities(Location centerPoint, Color colour, int width, int height, int outsideWidth, boolean immovable) {
+        List<RectangleEntity> entities = Lists.newArrayList();
+
+        for(int i = 0; i < outsideWidth; i++) {
+            Location nextPoint = centerPoint.subtract(i, i, 0);
+            Hitbox hitbox = new SquareLocationHitbox(nextPoint, new Dimension(width + 2*i, height + 2*i), immovable);
+
+            entities.add(new RectangleEntity(colour, nextPoint, hitbox.getDimensions(), true, immovable));
+        }
+
+        return entities.toArray(new RectangleEntity[0]);
+    }
+}
