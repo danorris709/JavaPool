4526ba807a1fb17bfab0561e20064ae61ab49b4e
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-04-29 19:24:54 +0100

    Ref: cue power handler

--- src/main/java/me/dnorris/pool/game/handler/CuePowerHandler.java
+++ src/main/java/me/dnorris/pool/game/handler/CuePowerHandler.java
@@ -1,28 +1,42 @@
 package me.dnorris.pool.game.handler;
 
+import me.dnorris.pool.arena.Entity;
 import me.dnorris.pool.arena.GameArena;
 import me.dnorris.pool.arena.key.KeyEventType;
 import me.dnorris.pool.arena.key.KeyHandler;
 import me.dnorris.pool.game.GameEntity;
 
+import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class CuePowerHandler {
 
     @KeyHandler(keyCode = KeyEvent.VK_UP, getType = KeyEventType.KEY_PRESSED)
-    public void onKeyPress(GameArena arena, KeyEvent event) {
-        GameEntity.getFullPowerBar().setLocation(GameEntity.getFullPowerBar().getLocation().subtract(0, 10, 0));
-        GameEntity.getFullPowerBar().getHitbox().getDimensions().setSize(
-                GameEntity.getFullPowerBar().getHitbox().getDimensions().getWidth(),
-                GameEntity.getFullPowerBar().getHitbox().getDimensions().getHeight() + 10
-        );
-        GameEntity.getFullPowerBar().setDirty(true);
-
-        GameEntity.getEmptyPowerBar().getHitbox().getDimensions().setSize(
-                GameEntity.getFullPowerBar().getHitbox().getDimensions().getWidth(),
-                GameEntity.getFullPowerBar().getHitbox().getDimensions().getHeight() - 10
-        );
-        GameEntity.getEmptyPowerBar().setDirty(true);
+    public void onUpArrowPressed(GameArena arena, KeyEvent event) {
+        this.updateFullPowerBarDimensions();
+        this.updateEmptyPowerBarDimensions();
     }
 
+    private void updateFullPowerBarDimensions() {
+        Entity fullPowerBar = GameEntity.getFullPowerBar();
+        Dimension fullDimensions = fullPowerBar.getHitbox().getDimensions();
+
+        fullPowerBar.setLocation(fullPowerBar.getLocation().subtract(0, 10, 0));
+        fullDimensions.setSize(
+                fullDimensions.getWidth(),
+                fullDimensions.getHeight() + 10
+        );
+        fullPowerBar.setDirty(true);
+    }
+
+    private void updateEmptyPowerBarDimensions() {
+        Entity emptyPowerBar = GameEntity.getEmptyPowerBar();
+        Dimension emptyDimensions = emptyPowerBar.getHitbox().getDimensions();
+
+        emptyPowerBar.getHitbox().getDimensions().setSize(
+                emptyDimensions.getWidth(),
+                emptyDimensions.getHeight() - 10
+        );
+        emptyPowerBar.setDirty(true);
+    }
 }
