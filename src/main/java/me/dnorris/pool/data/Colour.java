116a1c0a0c4f4cd327241c7a81f13f85b7c0ea09
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-04-29 15:06:21 +0100

    Ref: commented colour

--- src/main/java/me/dnorris/pool/data/Colour.java
+++ src/main/java/me/dnorris/pool/data/Colour.java
@@ -2,10 +2,25 @@ package me.dnorris.pool.data;
 
 import java.awt.*;
 
+/**
+ * Class used for storing colours that are commonly used as constants
+ *
+ * @author https://github.com/danorris709
+ */
 public class Colour {
 
+    /**
+     *
+     * The colour brown in the sRGB space
+     *
+     */
     public static final Color BROWN = new Color(100, 66, 32);
 
+    /**
+     *
+     * The colour dark green in the sRGB space
+     *
+     */
     public static final Color DARK_GREEN = new Color(0, 99, 0);
 
 }
