db176f1a088277043447f4944af09add084c449f
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-04-29 11:46:40 +0100

    Feat: added pool client start

--- src/main/java/me/dnorris/pool/PoolClient.java
+++ src/main/java/me/dnorris/pool/PoolClient.java
@@ -1,8 +1,24 @@
 package me.dnorris.pool;
 
+import me.dnorris.pool.arena.GameArena;
+import me.dnorris.pool.arena.arena.jframe.SimpleArena;
+import me.dnorris.pool.game.GameEntity;
+
+import java.awt.*;
+
 public class PoolClient {
 
     public static void main(String[] args) {
+        GameArena gameArena = new SimpleArena(new Dimension(1500, 750));
 
+        gameArena.addEntity(GameEntity.getBorder());
+        gameArena.addEntity(GameEntity.getCloth());
+        gameArena.addEntity(GameEntity.getHeadString());
+        gameArena.addEntity(GameEntity.getTopRightCornerHole());
+        gameArena.addEntity(GameEntity.getTopLeftCornerHole());
+        gameArena.addEntity(GameEntity.getTopMiddleHole());
+        gameArena.addEntity(GameEntity.getBottomMiddleHole());
+        gameArena.addEntity(GameEntity.getBottomRightCornerHole());
+        gameArena.addEntity(GameEntity.getBottomLeftCornerHole());
     }
 }
