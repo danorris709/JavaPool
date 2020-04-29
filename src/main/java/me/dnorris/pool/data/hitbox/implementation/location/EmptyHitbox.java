e7af91af14864983e97e192befe3be01ec1d9898
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-04-29 20:46:06 +0100

    Feat: Added empty hitbox

---
+++ src/main/java/me/dnorris/pool/data/hitbox/implementation/location/EmptyHitbox.java
@@ -0,0 +1,19 @@
+package me.dnorris.pool.data.hitbox.implementation.location;
+
+import me.dnorris.pool.data.hitbox.Hitbox;
+import me.dnorris.pool.data.hitbox.implementation.AbstractLocationHitbox;
+import me.dnorris.pool.data.location.Location;
+
+import java.awt.*;
+
+public class EmptyHitbox extends AbstractLocationHitbox {
+
+    public EmptyHitbox(Location center, boolean immovable) {
+        super(center, new Dimension(0, 0), immovable, false);
+    }
+
+    @Override
+    public boolean isColliding(Hitbox other) {
+        return false;
+    }
+}
