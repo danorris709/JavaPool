b3912b51672f24b1d894adf8f0a4155f7c579a78
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-04-29 18:45:37 +0100

    Feat: added coloured balls

--- src/main/java/me/dnorris/pool/PoolClient.java
+++ src/main/java/me/dnorris/pool/PoolClient.java
@@ -1,10 +1,15 @@
 package me.dnorris.pool;
 
+import me.dnorris.pool.arena.Entity;
 import me.dnorris.pool.arena.GameArena;
 import me.dnorris.pool.arena.arena.jframe.SimpleArena;
+import me.dnorris.pool.arena.entity.EntityBuilder;
+import me.dnorris.pool.arena.entity.EntityType;
 import me.dnorris.pool.game.GameEntity;
+import me.dnorris.pool.game.GameLocation;
 
 import java.awt.*;
+import java.lang.reflect.InvocationTargetException;
 
 public class PoolClient {
 
@@ -23,5 +28,27 @@ public class PoolClient {
         gameArena.addEntity(GameEntity.getBlackBallSpot());
         gameArena.addEntity(GameEntity.getCueBall());
         gameArena.addEntity(GameEntity.getBlackBall());
+
+        for(int i = 0; i < GameLocation.BALL_SPAWN_POINTS.length; i++) {
+            gameArena.addEntity(getBall(i));
+        }
+    }
+
+    private static Entity getBall(int position) {
+        try {
+            return new EntityBuilder()
+                    .setLocation(GameLocation.BALL_SPAWN_POINTS[position].getFirst())
+                    .setInteractable(true)
+                    .setImmovable(false)
+                    .setDimension(new Dimension(20, 20))
+                    .setHollow(false)
+                    .setColour(GameLocation.BALL_SPAWN_POINTS[position].getSecond())
+                    .setType(EntityType.CIRCLE)
+                    .build();
+        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
