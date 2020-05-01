383466070e7625f2e7c9504752c486d2f97f98eb
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-01 16:14:15 +0100

    Feat: commented Entity

--- src/main/java/me/dnorris/pool/arena/Entity.java
+++ src/main/java/me/dnorris/pool/arena/Entity.java
@@ -6,24 +6,84 @@ import me.dnorris.pool.data.vector.Vector;
 
 import java.awt.*;
 
+/**
+ *
+ * An interface that represents anything that can be added to the {@link GameArena}
+ *
+ * @author https://github.com/danorris709
+ */
 public interface Entity {
 
+    /**
+     *
+     * Gets the {@link Color} of the {@link Entity} on the {@link GameArena}
+     *
+     * @return The {@link Color} of the {@link Entity}
+     */
     Color getColour();
 
+    /**
+     *
+     * Gets the {@link Color} of the {@link Entity}
+     *
+     * @param colour {@link Color} of the {@link Entity}
+     */
     void setColour(Color colour);
 
+    /**
+     *
+     * Get the exact point at which the {@link Entity} lies
+     *
+     * @return The {@link Location} center of the {@link Entity}
+     */
     Location getLocation();
 
+    /**
+     *
+     * Set the exact point at which the {@link Entity} lies
+     *
+     * @param location The new {@link Location} center of the {@link Entity}
+     */
     void setLocation(Location location);
 
+    /**
+     *
+     * The physical representation of the {@link Entity} for when moving around the {@link GameArena}
+     *
+     * @return The {@link Hitbox} of the {@link Entity}
+     */
     Hitbox getHitbox();
 
+    /**
+     *
+     * The representation of the motion of the {@link Entity}
+     *
+     * @return The {@link Hitbox} of the {@link Entity}
+     */
     Vector getMotion();
 
+    /**
+     *
+     * Set the representation of the motion of the {@link Entity}
+     *
+     * @param motion The {@link Hitbox} of the {@link Entity}
+     */
     void setMotion(Vector motion);
 
+    /**
+     *
+     * Draws the entity onto the {@link GameArena}
+     *
+     * @param graphics The {@link Graphics2D} from the {@link GameArena#tick(Graphics2D)}
+     */
     void paint(Graphics2D graphics);
 
+    /**
+     *
+     * Updates the details of the {@link Entity} and is called every frame
+     * For example, updates the location of the {@link Entity} using the motion
+     *
+     */
     void tick();
 
 }
