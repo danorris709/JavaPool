f4f61b2498d8dff4d2295c0ee1d3e362e9de6ef9
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-01 15:33:25 +0100

    Ref: commented BiFunction interface

--- src/main/java/me/dnorris/pool/data/BiFunction.java
+++ src/main/java/me/dnorris/pool/data/BiFunction.java
@@ -1,7 +1,23 @@
 package me.dnorris.pool.data;
 
+/**
+ * Interface used for representation a function that takes two parameters but
+ * doesn't return anything.
+ *
+ * @param <A> The type of the first parameter of the function
+ * @param <B> The type of the second paramter of the function
+ * @author https://github.com/danorris709
+ */
 public interface BiFunction<A, B> {
 
+    /**
+     *
+     * Similar naming to {@link Runnable}. As can be represented by a lambda and thus there's no
+     * name that represents the function better.
+     *
+     * @param a The first object of type {@link A}
+     * @param b The second object of type {@link B}
+     */
     void run(A a, B b);
 
 }
