2f61701043dc22083d773224361314a412375002
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-02 16:52:30 +0100

    Feat: added collision checking.... yet to do physics

--- src/main/java/me/dnorris/pool/arena/arena/jframe/SimpleArena.java
+++ src/main/java/me/dnorris/pool/arena/arena/jframe/SimpleArena.java
@@ -3,9 +3,11 @@ package me.dnorris.pool.arena.arena.jframe;
 import me.dnorris.pool.arena.Entity;
 import me.dnorris.pool.arena.arena.AbstractGameArena;
 import me.dnorris.pool.arena.arena.jframe.listener.KeyHandlerListener;
+import me.dnorris.pool.data.vector.implementation.Vector2D;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class SimpleArena extends AbstractGameArena {
 
@@ -49,6 +51,17 @@ public class SimpleArena extends AbstractGameArena {
     public void tick(Graphics2D graphics) {
         synchronized (this.lock) {
             for (Entity entity : this.getEntities()) {
+                for(Entity other : this.getEntities()) {
+                    if(Objects.equals(other, entity) || other.getHitbox() == null || entity.getHitbox() == null) {
+                        continue;
+                    }
+
+                    if(other.getHitbox().isColliding(entity.getHitbox())) {
+                        other.setMotion(new Vector2D(10, 10));
+                        break;
+                    }
+                }
+
                 entity.paint(graphics);
                 entity.tick();
             }
