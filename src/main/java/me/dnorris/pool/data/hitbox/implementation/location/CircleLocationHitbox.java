090a376b83792a986af340fbe94417f767de2cba
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-02 16:02:35 +0100

    Feat: added circle location hitbox

---
+++ src/main/java/me/dnorris/pool/data/hitbox/implementation/location/CircleLocationHitbox.java
@@ -0,0 +1,25 @@
+package me.dnorris.pool.data.hitbox.implementation.location;
+
+import me.dnorris.pool.data.hitbox.Hitbox;
+import me.dnorris.pool.data.hitbox.implementation.AbstractLocationHitbox;
+import me.dnorris.pool.data.location.Location;
+
+import java.awt.*;
+
+public class CircleLocationHitbox extends AbstractLocationHitbox {
+
+    public CircleLocationHitbox(int priority, Location center, Dimension dimensions, boolean immovable, boolean interactable) {
+        super(priority, center, dimensions, immovable, interactable);
+    }
+
+    @Override
+    public Hitbox clone() {
+        return new CircleLocationHitbox(
+                this.getPriority(),
+                this.getCenter().clone(),
+                new Dimension(this.getDimensions().width, this.getDimensions().height),
+                this.isImmovable(),
+                this.isInteractable()
+        );
+    }
+}
