4aeec6e335b9695df63509d05d06c7897308fcb8
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-04 14:43:30 +0100

    reduced pause

--- src/main/java/me/dnorris/pool/arena/arena/jframe/SimpleArena.java
+++ src/main/java/me/dnorris/pool/arena/arena/jframe/SimpleArena.java
@@ -27,7 +27,7 @@ public class SimpleArena extends AbstractGameArena {
                 jFrame.repaint();
 
                 try {
-                    Thread.sleep(20);
+                    Thread.sleep(5);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
