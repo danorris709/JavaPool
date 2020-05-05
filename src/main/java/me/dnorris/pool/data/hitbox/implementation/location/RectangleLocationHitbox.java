126cc1f43409fee9e7a4ac9261eae645de7a44b6
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-05 18:46:28 +0100

    Added getLocation for RectangleLocationHitbox

--- src/main/java/me/dnorris/pool/data/hitbox/implementation/location/RectangleLocationHitbox.java
+++ src/main/java/me/dnorris/pool/data/hitbox/implementation/location/RectangleLocationHitbox.java
@@ -3,13 +3,26 @@ package me.dnorris.pool.data.hitbox.implementation.location;
 import me.dnorris.pool.data.hitbox.Hitbox;
 import me.dnorris.pool.data.hitbox.implementation.AbstractLocationHitbox;
 import me.dnorris.pool.data.location.Location;
+import me.dnorris.pool.data.location.implementation.Location2D;
 
 import java.awt.*;
 
 public class RectangleLocationHitbox extends AbstractLocationHitbox {
 
     public RectangleLocationHitbox(Location center, Dimension dimensions, boolean immovable, boolean interactable) {
-        super(1, center, dimensions, immovable, interactable);
+        super(1, center.add(dimensions.getWidth() / 2.0, dimensions.getHeight() / 2.0, 0), dimensions, immovable, interactable);
+    }
+
+    @Override
+    public Location getLocation(Hitbox other) {
+        Location otherCenter = other.getCenter();
+        Location thisCenter = this.getCenter();
+
+        if(otherCenter.getX() >= thisCenter.getX()) {
+            return new Location2D(otherCenter.getX() + (other.getDimensions().getWidth() / 2.0), other.getCenter().getY());
+        }
+
+        return new Location2D(otherCenter.getX() - (other.getDimensions().getWidth() / 2.0), other.getCenter().getY());
     }
 
     @Override
