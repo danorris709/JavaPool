385555f21824f1cc8ddabc17425c021eae21bdd5
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-07 11:30:43 +0100

    Commented EventHandler annotation

--- src/main/java/me/dnorris/pool/arena/event/listener/EventHandler.java
+++ src/main/java/me/dnorris/pool/arena/event/listener/EventHandler.java
@@ -5,10 +5,22 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ *
+ * Annotation for flagging functions as methods to handle events
+ *
+ * @author https://github.com/danorris709
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface EventHandler {
 
+    /**
+     *
+     * If the event handler function should ignore events that have been cancelled
+     *
+     * @return if the listener ignores cancelled events
+     */
     boolean ignoreCancelled() default true;
 
 }
