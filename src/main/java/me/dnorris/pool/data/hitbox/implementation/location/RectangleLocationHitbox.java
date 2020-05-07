dbdf097fb5fd96bfe27395a5f52a4b1682305432
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-07 11:03:43 +0100

    Commented RectangleLocationHitbox

--- src/main/java/me/dnorris/pool/data/hitbox/implementation/location/RectangleLocationHitbox.java
+++ src/main/java/me/dnorris/pool/data/hitbox/implementation/location/RectangleLocationHitbox.java
@@ -7,8 +7,24 @@ import me.dnorris.pool.data.location.implementation.Location2D;
 
 import java.awt.*;
 
+/**
+ *
+ * A rectangle implementation of the {@link AbstractLocationHitbox}
+ *
+ * @author https://github.com/danorris709
+ */
 public class RectangleLocationHitbox extends AbstractLocationHitbox {
 
+    /**
+     *
+     * Default constructor for Rectangle {@link AbstractLocationHitbox}
+     * Priority 2 as ball vs ball collisions don't work for ball vs rectangle
+     *
+     * @param center Center {@link Location} of the rectangle
+     * @param dimensions {@link Dimension} of the rectangle
+     * @param immovable if the ball can be moved
+     * @param interactable if the rectangle can be interacted with
+     */
     public RectangleLocationHitbox(Location center, Dimension dimensions, boolean immovable, boolean interactable) {
         super(2, center.add(dimensions.getWidth() / 2.0, dimensions.getHeight() / 2.0, 0), dimensions, immovable, interactable);
     }
