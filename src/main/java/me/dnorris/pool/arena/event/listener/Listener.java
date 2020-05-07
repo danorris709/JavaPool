ed1d4348863e3fec49b2c9c0c8ed75531e5ffe2c
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-07 11:29:56 +0100

    Commented Listener interface

--- src/main/java/me/dnorris/pool/arena/event/listener/Listener.java
+++ src/main/java/me/dnorris/pool/arena/event/listener/Listener.java
@@ -1,4 +1,11 @@
 package me.dnorris.pool.arena.event.listener;
 
+/**
+ *
+ * An interface that represents a class that can contain functions for handling
+ * when events are called
+ *
+ * @author https://github.com/danorris709
+ */
 public interface Listener {
 }
