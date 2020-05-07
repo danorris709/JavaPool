115675603064285f0c565bbf7c6b33ede2896055
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-07 11:28:22 +0100

    Commented Cancellable interface

--- src/main/java/me/dnorris/pool/arena/event/Cancellable.java
+++ src/main/java/me/dnorris/pool/arena/event/Cancellable.java
@@ -1,9 +1,27 @@
 package me.dnorris.pool.arena.event;
 
+/**
+ *
+ * An interface to represent an event that can be cancelled
+ *
+ * @author https://github.com/danorris709
+ */
 public interface Cancellable {
 
+    /**
+     *
+     * If the event has been cancelled
+     *
+     * @return true if cancelled - false otherwise
+     */
     boolean isCancelled();
 
+    /**
+     *
+     * Updated if the event has been cancelled
+     *
+     * @param cancelled true if cancelled - false otherwise
+     */
     void setCancelled(boolean cancelled);
 
 }
