48c571391d5579840f8e317dcd616eeb5a62e8ac
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-07 01:47:04 +0100

    Feat: Added EntityCollisionEvent

---
+++ src/main/java/me/dnorris/pool/arena/event/event/EntityCollisionEvent.java
@@ -0,0 +1,43 @@
+package me.dnorris.pool.arena.event.event;
+
+import me.dnorris.pool.arena.Entity;
+import me.dnorris.pool.arena.event.Cancellable;
+import me.dnorris.pool.arena.event.Event;
+import me.dnorris.pool.arena.event.handler.EventListener;
+
+public class EntityCollisionEvent implements Event, Cancellable {
+
+    private static final EventListener EVENT_HANDLER = new EventListener();
+
+    private Entity first;
+    private Entity second;
+    private boolean cancelled;
+
+    public EntityCollisionEvent(Entity first, Entity second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public Entity getFirst() {
+        return this.first;
+    }
+
+    public Entity getSecond() {
+        return this.second;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    @Override
+    public EventListener getHandler() {
+        return EVENT_HANDLER;
+    }
+}
