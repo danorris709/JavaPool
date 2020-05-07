a223b23f77c8869e0e5a4daa383ce44656926f56
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-07 11:33:06 +0100

    Commented EventListenerDetails DTO

--- src/main/java/me/dnorris/pool/arena/event/handler/EventListenerDetails.java
+++ src/main/java/me/dnorris/pool/arena/event/handler/EventListenerDetails.java
@@ -3,12 +3,26 @@ package me.dnorris.pool.arena.event.handler;
 import me.dnorris.pool.arena.event.Event;
 import me.dnorris.pool.data.Method;
 
+/**
+ *
+ * A DTO for details about an event listener
+ *
+ * @author https://github.com/danorris709
+ */
 public class EventListenerDetails {
 
     private final java.lang.reflect.Method declaredMethod;
     private final Method<Event> eventMethod;
     private final boolean ignoreCancelled;
 
+    /**
+     *
+     * Constructor for the DTO
+     *
+     * @param declaredMethod The method cached to easily remove in the future
+     * @param eventMethod The function called when the event is called
+     * @param ignoreCancelled If the event should be ignored if cancelled is true
+     */
     public EventListenerDetails(java.lang.reflect.Method declaredMethod, Method<Event> eventMethod, boolean ignoreCancelled) {
         this.declaredMethod = declaredMethod;
         this.eventMethod = eventMethod;
