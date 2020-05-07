1258808ca211bc8461cc98fc858a64b431f89ec8
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-07 01:34:11 +0100

    Fix: added correct annotations for annotations

--- src/main/java/me/dnorris/pool/arena/event/listener/EventHandler.java
+++ src/main/java/me/dnorris/pool/arena/event/listener/EventHandler.java
@@ -1,5 +1,12 @@
 package me.dnorris.pool.arena.event.listener;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface EventHandler {
 
     boolean ignoreCancelled();
