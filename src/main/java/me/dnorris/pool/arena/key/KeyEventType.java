d208296839031c89c7fd9b5d7050214e9b9f52a4
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-07 11:20:06 +0100

    Commented KeyEventType enum

--- src/main/java/me/dnorris/pool/arena/key/KeyEventType.java
+++ src/main/java/me/dnorris/pool/arena/key/KeyEventType.java
@@ -1,6 +1,13 @@
 package me.dnorris.pool.arena.key;
 
+/**
+ *
+ * Different types of key press for optimized handling of key press events
+ *
+ * @author https://github.com/danorris709
+ */
 public enum KeyEventType {
+
 
     KEY_TYPED(),
     KEY_RELEASED(),
