b85bd305fe886fe3c588f31e5018c93ed6e04f08
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-07 11:26:38 +0100

    Commented EventFactory

--- src/main/java/me/dnorris/pool/arena/event/EventFactory.java
+++ src/main/java/me/dnorris/pool/arena/event/EventFactory.java
@@ -4,14 +4,34 @@ import me.dnorris.pool.arena.Entity;
 import me.dnorris.pool.arena.event.event.EntityCollisionEvent;
 import me.dnorris.pool.arena.event.event.EntityStopMovingEvent;
 
+/**
+ *
+ * The static factory for calling events without polluting other logic further
+ *
+ * @author https://github.com/danorris709
+ */
 public class EventFactory {
 
+    /**
+     *
+     * Calling the {@link EntityCollisionEvent}
+     *
+     * @param first The first {@link Entity}
+     * @param second The second {@link Entity}
+     * @return The event created & called
+     */
     public static EntityCollisionEvent callCollisionEvent(Entity first, Entity second) {
         EntityCollisionEvent collisionEvent = new EntityCollisionEvent(first, second);
         Events.callEvent(collisionEvent);
         return collisionEvent;
     }
 
+    /**
+     *
+     * Calling the {@link EntityStopMovingEvent}
+     *
+     * @param entity The {@link Entity} that stopped moving
+     */
     public static void callStopMovingEvent(Entity entity) {
         Events.callEvent(new EntityStopMovingEvent(entity));
     }
