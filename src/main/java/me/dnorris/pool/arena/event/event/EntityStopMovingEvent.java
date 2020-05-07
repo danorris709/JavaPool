86f4b33b51a1949d150500f8c7cea735a34b5385
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-07 11:36:52 +0100

    Commented EntityStopMovingEvent

--- src/main/java/me/dnorris/pool/arena/event/event/EntityStopMovingEvent.java
+++ src/main/java/me/dnorris/pool/arena/event/event/EntityStopMovingEvent.java
@@ -4,12 +4,24 @@ import me.dnorris.pool.arena.Entity;
 import me.dnorris.pool.arena.event.Event;
 import me.dnorris.pool.arena.event.handler.EventListener;
 
+/**
+ *
+ * An event representing when an entity motion goes from a very small number to {@link me.dnorris.pool.data.vector.implementation.Vector2D#NONE}
+ *
+ * @author https://github.com/danorris709
+ */
 public class EntityStopMovingEvent implements Event {
 
     private static final EventListener EVENT_HANDLER = new EventListener();
 
-    private Entity entity;
+    private final Entity entity;
 
+    /**
+     *
+     * Constructor for the event giving the entity that's stopping moving
+     *
+     * @param entity The entity that stopped moving
+     */
     public EntityStopMovingEvent(Entity entity) {
         this.entity = entity;
     }
