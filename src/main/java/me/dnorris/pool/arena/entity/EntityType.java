456dd1cb3c6e26bd013427c876e528d70fa5df0b
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-07 11:42:59 +0100

    Commented EntityType

--- src/main/java/me/dnorris/pool/arena/entity/EntityType.java
+++ src/main/java/me/dnorris/pool/arena/entity/EntityType.java
@@ -9,6 +9,14 @@ import me.dnorris.pool.data.vector.Vector;
 import java.awt.*;
 import java.lang.reflect.Constructor;
 
+/**
+ *
+ * An enum representing the different types of buildable entity.
+ * Used by the {@link EntityBuilder} builder for finding the correct constructor
+ * in an abstract fashion without having to make a unique builder for each entity.
+ *
+ * @author https://github.com/danorris709
+ */
 public enum EntityType {
 
     RECTANGLE(RectangleEntity.class),
@@ -24,6 +32,12 @@ public enum EntityType {
 
     private Constructor<? extends Entity> entityConstructor = null;
 
+    /**
+     *
+     * Constructor for the entity type giving an entity class
+     *
+     * @param entityClass The class of the entity object
+     */
     EntityType(Class<? extends Entity> entityClass) {
         if(entityClass == null) {
             return;
@@ -36,6 +50,13 @@ public enum EntityType {
         }
     }
 
+    /**
+     *
+     * Get the constructor for the entity
+     *
+     * @return The constructor for the entity
+     * @throws UnsupportedOperationException If the constructor is null then the builder cannot be used for this entity type
+     */
     public Constructor<? extends Entity> getConstructor() throws UnsupportedOperationException {
         if(this.entityConstructor == null) {
             throw new UnsupportedOperationException("Use correct builder for this type");
