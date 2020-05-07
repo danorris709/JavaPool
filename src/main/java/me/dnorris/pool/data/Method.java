ba4e9b4d9ad03541a26579ca6f68789b01dda054
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-07 10:50:23 +0100

    Ref: commented method interface

--- src/main/java/me/dnorris/pool/data/Method.java
+++ src/main/java/me/dnorris/pool/data/Method.java
@@ -1,5 +1,13 @@
 package me.dnorris.pool.data;
 
+/**
+ *
+ * Interface used for representation a function that takes one parameter but
+ * doesn't return anything.
+ *
+ * @param <T> The type of the parameter for the function
+ * @author https://github.com/danorris709
+ */
 public interface Method<T> {
 
     void run(T t);
