c6164f51df1b5d11be0dfec446386d56f9d9e28b
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-07 11:06:42 +0100

    Commented GameFunction

--- src/main/java/me/dnorris/pool/arena/GameFunction.java
+++ src/main/java/me/dnorris/pool/arena/GameFunction.java
@@ -5,11 +5,24 @@ import me.dnorris.pool.data.BiFunction;
 
 import java.awt.event.KeyEvent;
 
+/**
+ *
+ * Immutable object used by the {@link GameArena} for {@link me.dnorris.pool.arena.key.KeyHandler} annotations
+ *
+ * @author https://github.com/danorris709
+ */
 public class GameFunction {
 
     private final KeyEventType eventType;
     private final BiFunction<GameArena, KeyEvent> function;
 
+    /**
+     *
+     * Basic constructor setting the variables
+     *
+     * @param eventType The {@link KeyEventType} type
+     * @param function The {@link BiFunction} executed once the key is pressed
+     */
     public GameFunction(KeyEventType eventType, BiFunction<GameArena, KeyEvent> function) {
         this.eventType = eventType;
         this.function = function;
