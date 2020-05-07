ce408614264be683ab305976be1acea3cd2c5b41
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-07 01:46:49 +0100

    Feat: Added registering listeners in static factory

--- src/main/java/me/dnorris/pool/arena/event/Events.java
+++ src/main/java/me/dnorris/pool/arena/event/Events.java
@@ -1,14 +1,53 @@
 package me.dnorris.pool.arena.event;
 
+import com.google.common.collect.Maps;
+import me.dnorris.pool.arena.event.handler.EventListener;
+import me.dnorris.pool.arena.event.handler.EventListenerDetails;
 import me.dnorris.pool.arena.event.listener.Listener;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
 public class Events {
-    
+
+    private static final Map<Class<? extends Event>, EventListener> EVENT_LISTENER_CACHE = Maps.newHashMap();
+
     public static void callEvent(Event event) {
-        // TODO: 06/05/2020
+        for (EventListenerDetails listener : event.getHandler().getListeners()) {
+            listener.getEventMethod().run(event);
+        }
     }
 
+    @SuppressWarnings("unchecked")
     public static void registerListener(Listener listener) {
-        // TODO: 06/05/2020
+        for (Method declaredMethod : listener.getClass().getDeclaredMethods()) {
+            me.dnorris.pool.arena.event.listener.EventHandler eventHandler = declaredMethod.getAnnotation(me.dnorris.pool.arena.event.listener.EventHandler.class);
+
+            if(eventHandler == null) {
+                continue;
+            }
+
+            Class<? extends Event> event = (Class<? extends Event>) declaredMethod.getParameterTypes()[0];
+            EventListener eventListener = getHandlers(event);
+
+            eventListener.addListener(listener, eventHandler, declaredMethod);
+        }
+    }
+
+    private static EventListener getHandlers(Class<? extends Event> clazz) {
+        return EVENT_LISTENER_CACHE.computeIfAbsent(clazz, ___ -> {
+            try {
+                Method declaredMethod = clazz.getDeclaredMethod("getHandler");
+
+                declaredMethod.setAccessible(true);
+
+                return (EventListener) declaredMethod.invoke(null);
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        });
     }
 }
