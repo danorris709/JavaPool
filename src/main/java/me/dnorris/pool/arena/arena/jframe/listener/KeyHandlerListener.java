0aeb07e49e8c5ac6060b53784aafec0ab0bd7a1e
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-08 12:58:58 +0100

    Commented KeyHandlerListener

--- src/main/java/me/dnorris/pool/arena/arena/jframe/listener/KeyHandlerListener.java
+++ src/main/java/me/dnorris/pool/arena/arena/jframe/listener/KeyHandlerListener.java
@@ -7,10 +7,23 @@ import me.dnorris.pool.arena.key.KeyEventType;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+/**
+ *
+ * Listener used by the {@link me.dnorris.pool.arena.GameArena} for calling the
+ * {@link me.dnorris.pool.arena.key.KeyHandler} implementations when a key is pressed
+ *
+ * @author https://github.com/danorris709
+ */
 public class KeyHandlerListener implements KeyListener {
 
     private final SimpleArena gameArena;
 
+    /**
+     *
+     * Basic constructor for a game arena
+     *
+     * @param gameArena The game arena checking for key presses
+     */
     public KeyHandlerListener(SimpleArena gameArena) {
         this.gameArena = gameArena;
     }
