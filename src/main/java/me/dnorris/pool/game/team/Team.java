38be003cbd1b25e3b38c04fde4390b1592117df7
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-08 19:27:44 +0100

    Added display text to teams

--- src/main/java/me/dnorris/pool/game/team/Team.java
+++ src/main/java/me/dnorris/pool/game/team/Team.java
@@ -2,9 +2,19 @@ package me.dnorris.pool.game.team;
 
 public enum Team {
 
-    PLAYER_ONE(),
-    PLAYER_TWO(),
+    PLAYER_ONE("PLAYER 1's TURN"),
+    PLAYER_TWO("PLAYER 2's TURN"),
 
     ;
+
+    private final String displayText;
+
+    Team(String displayText) {
+        this.displayText = displayText;
+    }
+
+    public String getDisplayText() {
+        return this.displayText;
+    }
 
 }
