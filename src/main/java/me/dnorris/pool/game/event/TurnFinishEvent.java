8103131fc3fc207b1d3e45c462f4eb7ad5587111
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-08 20:27:27 +0100

    Added active game to TurnFinishEvent

--- src/main/java/me/dnorris/pool/game/event/TurnFinishEvent.java
+++ src/main/java/me/dnorris/pool/game/event/TurnFinishEvent.java
@@ -3,6 +3,7 @@ package me.dnorris.pool.game.event;
 import me.dnorris.pool.arena.Entity;
 import me.dnorris.pool.arena.event.Event;
 import me.dnorris.pool.arena.event.handler.EventListener;
+import me.dnorris.pool.game.GameData;
 import me.dnorris.pool.game.team.Team;
 
 import java.util.List;
@@ -11,14 +12,20 @@ public class TurnFinishEvent implements Event {
 
     private static final EventListener EVENT_HANDLER = new EventListener();
 
+    private final GameData activeGame;
     private final Team turn;
     private final Entity firstCollision;
     private final List<Entity> pottedBalls;
 
-    public TurnFinishEvent(Team turn, Entity firstCollision, List<Entity> pottedBalls) {
+    public TurnFinishEvent(GameData activeGame, Team turn, Entity firstCollision, List<Entity> pottedBalls) {
+        this.activeGame = activeGame;
         this.turn = turn;
         this.firstCollision = firstCollision;
         this.pottedBalls = pottedBalls;
+    }
+
+    public GameData getActiveGame() {
+        return this.activeGame;
     }
 
     public Team getTurn() {
