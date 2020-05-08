1fefcc5586a9ce93c8f7727c0fe6d648550c6acd
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-08 20:21:19 +0100

    Added BallCollisionListener for BallCollideEvent implementation

---
+++ src/main/java/me/dnorris/pool/game/listener/BallCollisionListener.java
@@ -0,0 +1,27 @@
+package me.dnorris.pool.game.listener;
+
+import me.dnorris.pool.arena.entity.shape.CircleEntity;
+import me.dnorris.pool.arena.event.Events;
+import me.dnorris.pool.arena.event.event.EntityCollisionEvent;
+import me.dnorris.pool.arena.event.listener.EventHandler;
+import me.dnorris.pool.arena.event.listener.Listener;
+import me.dnorris.pool.game.GameData;
+import me.dnorris.pool.game.event.BallCollideEvent;
+
+public class BallCollisionListener implements Listener {
+
+    private final GameData currentGame;
+
+    public BallCollisionListener(GameData currentGame) {
+        this.currentGame = currentGame;
+    }
+
+    @EventHandler
+    public void onEntityCollide(EntityCollisionEvent event) {
+        if (!(event.getFirst() instanceof CircleEntity) || !(event.getSecond() instanceof CircleEntity)) {
+            return;
+        }
+
+        Events.callEvent(new BallCollideEvent(this.currentGame, event.getFirst(), event.getSecond()));
+    }
+}
