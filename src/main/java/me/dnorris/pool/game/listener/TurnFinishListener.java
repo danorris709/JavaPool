ed3a1ec4cb263fd3597c8a9c2977e1778cd0b52e
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-09 19:12:46 +0100

    Implemented turn switching and basic rules

--- src/main/java/me/dnorris/pool/game/listener/TurnFinishListener.java
+++ src/main/java/me/dnorris/pool/game/listener/TurnFinishListener.java
@@ -1,9 +1,14 @@
 package me.dnorris.pool.game.listener;
 
+import me.dnorris.pool.arena.Entity;
 import me.dnorris.pool.arena.event.listener.EventHandler;
 import me.dnorris.pool.arena.event.listener.Listener;
 import me.dnorris.pool.game.GameData;
 import me.dnorris.pool.game.event.TurnFinishEvent;
+import me.dnorris.pool.game.team.Team;
+
+import java.awt.*;
+import java.util.Objects;
 
 public class TurnFinishListener implements Listener {
 
@@ -15,6 +20,74 @@ public class TurnFinishListener implements Listener {
 
     @EventHandler
     public void onTurnFinish(TurnFinishEvent event) {
-        // TODO: 08/05/2020 rule logic
+        Team turn = event.getTurn();
+        Color color = currentGame.getTeamColour(turn);
+        GameData activeGame = event.getActiveGame();
+
+        if(event.getFirstCollision() == null) {
+            activeGame.setTurn(turn.getOpposition());
+            activeGame.setShotsInTurn(2);
+            return;
+        }
+
+        if(color == null) {
+            if(!event.getPottedBalls().isEmpty()) {
+                Color pottedColour = this.getPottedColour(event);
+
+                if(pottedColour == null) {
+                    return;
+                }
+
+                activeGame.setTeamColour(turn, pottedColour);
+                activeGame.setTeamColour(turn.getOpposition(), this.getOtherColour(pottedColour));
+            }
+        }else {
+            if(this.pottedOtherTeamsBall(event, color)) {
+                activeGame.setTurn(turn.getOpposition());
+                activeGame.setShotsInTurn(2);
+                return;
+            }
+        }
+
+        if(event.getPottedBalls().isEmpty()) {
+            activeGame.setShotsInTurn(activeGame.getShotsInTurn() - 1);
+        }
+
+        if(activeGame.getShotsInTurn() <= 0) {
+            activeGame.setTurn(turn.getOpposition());
+            activeGame.setShotsInTurn(1);
+        }
+    }
+
+    private boolean pottedOtherTeamsBall(TurnFinishEvent event, Color colour) {
+        for (Entity pottedBall : event.getPottedBalls()) {
+            if(!Objects.equals(pottedBall.getColour(), colour)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private Color getPottedColour(TurnFinishEvent event) {
+        Color colour = null;
+
+        for (Entity pottedBall : event.getPottedBalls()) {
+            if(colour != null && !Objects.equals(pottedBall.getColour(), colour)) {
+                return null;
+            }
+
+            colour = pottedBall.getColour();
+        }
+
+        return colour;
+    }
+
+    private Color getOtherColour(Color color) {
+        if(Objects.equals(color, Color.YELLOW)) {
+            return Color.RED;
+        }
+
+        return Color.YELLOW;
     }
 }
