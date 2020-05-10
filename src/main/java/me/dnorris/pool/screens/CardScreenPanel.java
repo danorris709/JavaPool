0bc504ba3e1c99a6d7b2b5305f065fd1c15ed087
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-11 00:52:28 +0100

    Commented CardScreenPanel

--- src/main/java/me/dnorris/pool/screens/CardScreenPanel.java
+++ src/main/java/me/dnorris/pool/screens/CardScreenPanel.java
@@ -2,11 +2,23 @@ package me.dnorris.pool.screens;
 
 import javax.swing.*;
 
+/**
+ *
+ * Simple base class for any panels used
+ *
+ * @author https://github.com/danorris709
+ */
 public class CardScreenPanel {
 
     private final String identifier;
     private final JPanel jPanel = new JPanel();
 
+    /**
+     *
+     * Basic constructor taking the identifier used for the card
+     *
+     * @param identifier identifier for the card
+     */
     public CardScreenPanel(String identifier) {
         this.identifier = identifier;
     }
