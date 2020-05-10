0333b645c0ef4f3152d5abcf35ab2742f65d61db
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-10 15:14:08 +0100

    Added getter for screen manager

--- src/main/java/me/dnorris/pool/PoolClient.java
+++ src/main/java/me/dnorris/pool/PoolClient.java
@@ -4,10 +4,16 @@ import me.dnorris.pool.screens.ScreenManager;
 
 public class PoolClient {
 
+    private static ScreenManager screenManager;
+
     public static void main(String[] args) {
-        ScreenManager screenManager = new ScreenManager();
+        screenManager = new ScreenManager();
 
         screenManager.initFrame();
         screenManager.openHomeScreen();
+    }
+
+    public static ScreenManager getScreenManager() {
+        return screenManager;
     }
 }
