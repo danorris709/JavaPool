3713de9332b4a381422d5ad592e30165609ec316
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-11 17:54:38 +0100

    Added get password function and get user function

--- Server/src/main/java/me/dnorris/server/user/UserDatabaseController.java
+++ Server/src/main/java/me/dnorris/server/user/UserDatabaseController.java
@@ -1,9 +1,11 @@
 package me.dnorris.server.user;
 
 import me.dnorris.server.database.DatabaseController;
+import me.dnorris.server.user.data.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDatabaseController {
@@ -12,9 +14,12 @@ public class UserDatabaseController {
             "id             INT             NOT NULL        AUTO_INCREMENT, " +
             "username       VARCHAR(64)     NOT NULL        UNIQUE, " +
             "email          LONGTEXT        NOT NULl        UNIQUE, " +
-            "name           LONGTEXT        NOT NULL, " +
+            "user_name      LONGTEXT        NOT NULL, " +
             "password       LONGTEXT        NOT NULL, " +
             "PRIMARY KEY(id));";
+
+    private static final String SELECT_USER = "SELECT * FROM `users` WHERE username = LOWER(?);";
+    private static final String SELECT_PASSWORD = "SELECT password FROM `users` WHERE username = LOWER(?);";
 
     private final DatabaseController databaseController;
 
@@ -32,5 +37,49 @@ public class UserDatabaseController {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public User getUser(String username) {
+        try (Connection connection = this.databaseController.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER);
+
+            preparedStatement.setString(1, username);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if(!resultSet.next()) {
+                return null;
+            }
+
+            return new User(resultSet.getLong("id"),
+                    username,
+                    resultSet.getString("email"),
+                    resultSet.getString("user_name"),
+                    resultSet.getString("password"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String getPassword(String username) {
+        try (Connection connection = this.databaseController.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_PASSWORD);
+
+            preparedStatement.setString(1, username);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if(!resultSet.next()) {
+                return null;
+            }
+
+            return resultSet.getString("password");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
