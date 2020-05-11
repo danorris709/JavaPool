eff7808bdbe5937da893f6a19f172538ad72909c
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-05-11 04:50:14 +0100

    Updated run.bat to return to original directory

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-cd CoolPool/ && ..\gradlew.bat clean shadowJar javadoc && java -jar build/libs/PoolProject.jar
+cd CoolPool/ && ..\gradlew.bat clean shadowJar javadoc && java -jar build/libs/PoolProject.jar && cd ../
